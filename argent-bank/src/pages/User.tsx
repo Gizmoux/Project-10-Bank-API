2e823d0063fdbb2e96894a196db20952b5244423
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-06-07 15:40:26 +0200

    ✅ Add edit name

--- argent-bank/src/pages/User.tsx
+++ argent-bank/src/pages/User.tsx
@@ -1,18 +1,64 @@
 import '../assets/css/main.css';
-import { useSelector } from 'react-redux';
-import { RootState } from '../app/store';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUserProfile } from '../app/authSlice';
 const User: React.FC = () => {
-	const { firstName, lastName } = useSelector((state: RootState) => state.auth);
+	const { token, firstName, lastName } = useSelector(state => state.auth);
+	const [editFirstName, setEditFirstName] = useState('');
+	const [editLastName, setEditLastName] = useState('');
+	const [isEditing, setIsEditing] = useState(false);
+	const [isSaving, setIsSaving] = useState(false);
+	const dispatch = useDispatch();
+
+	const handleChangeFirstName = event => {
+		setEditFirstName(event.target.value);
+		console.log(event.target.value);
+	};
+	const handleChangeLastName = event => {
+		setEditLastName(event.target.value);
+		console.log(event.target.value);
+	};
+	const handleEdit = () => {
+		setIsEditing(true);
+	};
+	const handleSave = async () => {
+		setIsSaving(true);
+		await dispatch(updateUserProfile(editFirstName, editLastName));
+		setIsSaving(false);
+		setIsEditing(false);
+	};
 	return (
 		<main className="main bg-dark">
-			<div className="header">
-				<h1>
-					Welcome back
+			{isEditing ? (
+				<div className="header">
+					<h1>Welcome back</h1>
+					<input
+						type="text"
+						placeholder="Tony"
+						onChange={handleChangeFirstName}
+					/>
+					<input
+						type="text"
+						placeholder="Jarvis"
+						onChange={handleChangeLastName}
+					/>
 					<br />
-					debut {firstName} {lastName} fin
-				</h1>
-				<button className="edit-button">Edit Name</button>
-			</div>
+
+					<button className="save-button" onClick={handleSave}>
+						Save
+					</button>
+					<button className="cancel-button">Cancel</button>
+				</div>
+			) : (
+				<div className="header">
+					<h1>Welcome back</h1>
+
+					<button className="edit-button" onClick={handleEdit}>
+						Edit Name
+					</button>
+				</div>
+			)}
+
 			<h2 className="sr-only">Accounts</h2>
 			<section className="account">
 				<div className="account-content-wrapper">
